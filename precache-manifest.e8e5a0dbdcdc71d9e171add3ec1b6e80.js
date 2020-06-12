ee5720cf3524a09a1655323323a160d08e08b7e0
ablashch <ablashch@gmail.com>
2020-06-12 16:22:05 +0300

    Updates

---
+++ precache-manifest.e8e5a0dbdcdc71d9e171add3ec1b6e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78f7fe675c5ee6c430370b3cb22b8e7",
+    "url": "/IF-Weather-APP/index.html"
+  },
+  {
+    "revision": "3c6d86f0f2603a0c51f3",
+    "url": "/IF-Weather-APP/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "60f3c8c72fdceaed1f26",
+    "url": "/IF-Weather-APP/static/js/2.3e93ceb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IF-Weather-APP/static/js/2.3e93ceb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6d86f0f2603a0c51f3",
+    "url": "/IF-Weather-APP/static/js/main.21882afa.chunk.js"
+  },
+  {
+    "revision": "85e5a4a39a18e47f0753",
+    "url": "/IF-Weather-APP/static/js/runtime-main.2a703517.js"
+  }
+]);
